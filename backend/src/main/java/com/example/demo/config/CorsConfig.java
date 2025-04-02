3bd79ac91bc45c905d6f613260ed5457e4d0938e
K4SUND <kasundilumina2016@gmail.com>
2025-04-03 01:07:10 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Change for production http://localhost
+                        .allowedOrigins("http://localhost") // Change for production http://localhost
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
